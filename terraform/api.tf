969008296c9ae463c838479a617895fab4cd00ab
Tyler Hale <7747991+t-hale@users.noreply.github.com>
2026-01-20 21:47:41 -0500

    --amend

--- terraform/api.tf
+++ terraform/api.tf
@@ -3,6 +3,11 @@ resource "google_project_service" "artifact_registry" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "calendar" {
+  service = "calendar-json.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "cloud_build" {
   service            = "cloudbuild.googleapis.com"
   disable_on_destroy = false
