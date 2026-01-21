92c044792e805945a37332b6312c65b95310e07c
Tyler Hale <7747991+t-hale@users.noreply.github.com>
2026-01-20 21:34:14 -0500

    --amend

---
+++ terraform/api.tf
@@ -0,0 +1,19 @@
+resource "google_project_service" "artifact_registry" {
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloud_build" {
+  service            = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloud_run" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "infrastructure_manager" {
+  service = "config.googleapis.com"
+  disable_on_destroy = false
+}
