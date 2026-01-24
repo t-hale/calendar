0388f13c67024e3275be95bfa3e1648e5ff540dc
Tyler Hale <7747991+t-hale@users.noreply.github.com>
2026-01-23 22:46:03 -0500

    --amend

--- scripts/deploy_infra.sh
+++ scripts/deploy_infra.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+printenv
+
 gcloud auth list
 gcloud config set project $PROJECT_NUMBER
 gcloud auth login
 gcloud container images describe $_DOCKER_IMAGE --format="value(image_summary.digest)" > image.txt
-gcloud infra-manager deployments apply $_DEPLOYMENT --service-account=$_SERVICE_ACCOUNT --git-source-repo=$_GITHUB_REPO --git-source-directory=terraform --git-source-ref=master --variables="image_tag=$(cat image.txt)'
+gcloud infra-manager deployments apply $_DEPLOYMENT \
+  --service-account=$_SERVICE_ACCOUNT \
+  --git-source-repo=$_GITHUB_REPO \
+  --git-source-directory=terraform \
+  --git-source-ref=master \
+  --variables="image_tag=$(cat image.txt)"
