a468000be8e034044c1bc7d0aebb1f40be40ac62
Tyler Hale <7747991+t-hale@users.noreply.github.com>
2026-01-24 20:39:00 -0500

    --amend

--- scripts/deploy_infra.sh
+++ scripts/deploy_infra.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-gcloud infra-manager deployments apply $_DEPLOYMENT \
-  --service-account=$_SERVICE_ACCOUNT \
-  --git-source-repo=$_GITHUB_REPO \
-  --git-source-directory=terraform \
-  --git-source-ref=master
+usage() {
+  echo "Usage: $0 --deployment <deployment_name> --service_account <email> --github_repo <repo_url> --git_source_directory <dir> --git_source_ref <branch/ref>"
+  exit 1
+}
+
+# Parse named arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --deployment) _DEPLOYMENT="$2"; shift ;;
+    --service_account) _SERVICE_ACCOUNT="$2"; shift ;;
+    --github_repo) _GITHUB_REPO="$2"; shift ;;
+    --git_source_directory) _GIT_SOURCE_DIRECTORY="$2"; shift ;;
+    --git_source_ref) _GIT_SOURCE_REF="$2"; shift ;;
+    *) echo "Unknown parameter: $1"; usage ;;
+  esac
+  shift
+done
+
+# Fail if any parameter is missing
+if [[ -z "$_DEPLOYMENT" ]] || [[ -z "$_SERVICE_ACCOUNT" ]] || [[ -z "$_GITHUB_REPO" ]] || [[ -z "$_GIT_SOURCE_DIRECTORY" ]] || [[ -z "$_GIT_SOURCE_REF" ]]; then
+  echo "Error: Missing required arguments."
+  usage
+fi
+
+gcloud infra-manager deployments apply "$_DEPLOYMENT" \
+  --service-account="$_SERVICE_ACCOUNT" \
+  --git-source-repo="$_GITHUB_REPO" \
+  --git-source-directory="$_GIT_SOURCE_DIRECTORY" \
+  --git-source-ref="$_GIT_SOURCE_REF"
